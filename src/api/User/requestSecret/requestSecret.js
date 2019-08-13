c5a9c67c6848486b334aa4ff22651885ebc14314
superbeom <it.super.beom@gmail.com>
2019-08-13 20:43:09 +0900

    #3.3 sendMail Function with Nodemailer

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,5 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { generateSecret } from "../../../utils";
+import { generateSecret, sendSecretMail } from "../../../utils";
 
 export default {
   Mutation: {
@@ -9,6 +9,7 @@ export default {
       console.log(loginSecret);
 
       try {
+        await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (error) {
