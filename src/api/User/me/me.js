61da31e35b95baa9e3b5871538d3199604ed67c8
superbeom <it.super.beom@gmail.com>
2019-08-21 17:03:52 +0900

    #3.12 me Resolver

---
+++ src/api/User/me/me.js
@@ -0,0 +1,12 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    me: (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+
+      return prisma.user({ id: user.id });
+    }
+  }
+};
