089ee85d80e894fd36acf2ed46c6b9ba4cc012d1
superbeom <it.super.beom@gmail.com>
2019-08-21 23:03:17 +0900

    #3.15 isFollowing and isSelf Computed Field in type User

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -9,6 +9,36 @@ export default {
     comments: ({ id }) => prisma.user({ id }).comments(),
     rooms: ({ id }) => prisma.user({ id }).rooms(),
 
-    fullName: parent => `${parent.firstName} ${parent.lastName}`
+    fullName: parent => `${parent.firstName} ${parent.lastName}`,
+
+    isFollowing: (parent, _, { request }) => {
+      const { id: parentId } = parent;
+      const { user } = request;
+
+      try {
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: {
+                id: parentId
+              }
+            }
+          ]
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+
+    isSelf: (parent, _, { request }) => {
+      const { id: parentId } = parent;
+      const { user } = request;
+
+      return user.id === parentId;
+    }
   }
 };
