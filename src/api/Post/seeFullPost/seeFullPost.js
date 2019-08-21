585f18ca49efaafcdb29c4a40a7f3fd683e82ab2
superbeom <it.super.beom@gmail.com>
2019-08-21 17:17:30 +0900

    #3.13 seeFullPost Resolver

---
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -0,0 +1,11 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeFullPost: (_, args) => {
+      const { id } = args;
+
+      return prisma.post({ id });
+    }
+  }
+};
