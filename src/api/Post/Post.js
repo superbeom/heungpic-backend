8f832d598511bc6dba3e4b2454782290486341e6
superbeom <it.super.beom@gmail.com>
2019-08-22 22:14:58 +0900

    #3.17 isLiked / likeCount / commentCount Computed Field in type Post

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -5,6 +5,50 @@ export default {
     user: ({ id }) => prisma.post({ id }).user(),
     files: ({ id }) => prisma.post({ id }).files(),
     likes: ({ id }) => prisma.post({ id }).likes(),
-    comments: ({ id }) => prisma.post({ id }).comments()
+    comments: ({ id }) => prisma.post({ id }).comments(),
+
+    isLiked: (parent, _, { request }) => {
+      const { id: postId } = parent;
+      const { user } = request;
+
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id: postId
+            }
+          }
+        ]
+      });
+    },
+
+    likeCount: parent =>
+      prisma
+        .likesConnection({
+          where: {
+            post: {
+              id: parent.id
+            }
+          }
+        })
+        .aggregate()
+        .count(),
+
+    commentCount: parent =>
+      prisma
+        .commentsConnection({
+          where: {
+            post: {
+              id: parent.id
+            }
+          }
+        })
+        .aggregate()
+        .count()
   }
 };
