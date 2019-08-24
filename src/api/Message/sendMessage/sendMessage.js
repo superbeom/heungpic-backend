7ab435d0ff8b43ea691408f53004632e7d412226
superbeom <it.super.beom@gmail.com>
2019-08-24 11:34:25 +0900

    #3.23 sendMessage Resolover part One

---
+++ src/api/Message/sendMessage/sendMessage.js
@@ -0,0 +1,28 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    sendMessage: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { roomId, message, toId } = args;
+      const { user } = reqest;
+      let room;
+
+      if (roomId === undefined) {
+        if (user.id !== toId) {
+          room = await prisma.createRoom({
+            participants: {
+              connect: [{ id: user.id }, { id: toId }]
+            }
+          });
+        }
+      } else {
+        room = await prisma.room({ id: roomId });
+      }
+
+      if (!room) {
+        throw Error("Room not found");
+      }
+    }
+  }
+};
